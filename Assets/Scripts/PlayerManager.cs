ee0b9b579a6d3bca4232693603ca31b764f28248
Abdullah Enes Kocabuğa <75130813+dev-aek@users.noreply.github.com>
2023-10-14 01:55:16 +0300

    hareket

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private Transform cam;
+    private float turnSpeedTime = .1f;
+    private float turnSmoothVelocity;
+    Rigidbody rgb;
+    CharacterController controller;
+    Animator animator;
+
+    public float jumpHeight;
+    public float gravity;
+    bool isGrounded;
+    Vector3 velocity;
+
+    private void Awake()
+    {
+        rgb = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        isGrounded = Physics.CheckSphere(transform.position, .1f, 1);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -1; 
+        }
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSpeedTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+        //jumping
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt((jumpHeight * 10) * -2f * gravity);
+        }
+
+        if (velocity.y > -20)
+        {
+            velocity.y += (gravity * 10) * Time.deltaTime;
+        }
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
